6506040dc1f32d26e882510fd286e52132e3ff64
tennesseee <antdvgi@gmail.com>
2021-10-29 11:44:28 +0300

    Add project files.

---
+++ IoTApp/Item.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+
+namespace IoTApp
+{
+    class Item
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public int Amount { get; set; }
+
+        [JsonConstructor]
+        public Item (int id, string itemName, int amount)
+        {
+            Id = id;
+            ItemName = itemName;
+            Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return "Id " + this.Id + "ItemName " + this.ItemName + "Amount " + this.Amount;
+        }
+    }
+}
---
+++ IoTApp/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IoTApp
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        
+        static async Task Main(string[] args)
+        {
+            await RunAsync();
+            while (true)
+            {
+                Get();
+
+            }        }
+
+        private static ConcurrentBag<Item> GetItems()
+        {
+            var action = $"/Items/Get";
+            var request =
+                client.GetAsync(action);
+
+            var response =
+                request.Result.Content.
+                ReadAsAsync<ConcurrentBag<Item>>();
+
+            return response.Result;
+        }
+
+        static void Get()
+        {
+            //client.BaseAddress = new Uri("https://localhost:44310/");
+            //client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(
+            //    new MediaTypeWithQualityHeaderValue("aplication/json"));
+
+            try
+            {
+                // read
+                Console.WriteLine("List of items:");
+                ConcurrentBag<Item> items = GetItems();
+                foreach (var item in items)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri("https://localhost:44310/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("aplication/json"));
+
+            try
+            {
+                for (int i = 0; i < 5000; i++)
+                {
+                    Item item = new Item(i, $"{i} element", i*i);
+                    var url = await CreateItemAsync(item); 
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex); ;
+            }
+        }
+
+        static async Task<Uri> CreateItemAsync(Item item)
+        {
+            HttpResponseMessage responseMessage = await client.PostAsJsonAsync("Items/Post", item);
+            responseMessage.EnsureSuccessStatusCode();
+
+            return responseMessage.Headers.Location;
+        }
+    }
+}
